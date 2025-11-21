2ced896c9799fd32ca4530203a64a37c642e1310
Danii9790 <daniyalashraf9790@gmail.com>
2025-11-21 12:26:58 +0500

    Update

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   experimental: {
     serverActions: {
       // Allow server actions from all origins for development
